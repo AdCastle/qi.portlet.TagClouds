ae2737163ef3b9f74dd64f24c8108dee0e57bd1a
Adam Castle <acastle@ripe.net>
2016-08-12 11:56:05 +0200

    setting up plone.app.testing

--- src/qi/portlet/TagClouds/testing.py
+++ src/qi/portlet/TagClouds/testing.py
@@ -1,18 +1,32 @@
-from Products.Five import zcml
-from Products.Five import fiveconfigure
-from Products.PloneTestCase.PloneTestCase import installPackage
-from collective.testcaselayer.ptc import ptc_layer, BasePTCLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import applyProfile
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
 
 
-class TagCloudLayer(BasePTCLayer):
-    """ set up basic testing layer """
+class TagCloudLayer(PloneSandboxLayer):
 
-    def afterSetUp(self):
-        fiveconfigure.debug_mode = True
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML for this package + dependencies
         import qi.portlet.TagClouds
-        zcml.load_config('configure.zcml', qi.portlet.TagClouds)
-        fiveconfigure.debug_mode = False
-        installPackage('qi.portlet.TagClouds', quiet=True)
-        self.addProduct('qi.portlet.TagClouds')
+        xmlconfig.file('configure.zcml',
+                       qi.portlet.TagClouds,
+                       context=configurationContext)
+        z2.installProduct(app, 'qi.portlet.TagClouds')
 
-layer = TagCloudLayer(bases=[ptc_layer])
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'Products.CMFPlone:plone')
+
+TAGCLOUD_FIXTURE = TagCloudLayer()
+TAGCLOUD_INTEGRATION_TESTING = \
+    IntegrationTesting(bases=(TAGCLOUD_FIXTURE, ),
+                       name="qi.portlet.tagcloud:Integration")
+TAGCLOUD_FUNCTIONAL_TESTING = \
+    FunctionalTesting(bases=(TAGCLOUD_FIXTURE, ),
+                      name="qi.portlet.tagcloud:Functional")
