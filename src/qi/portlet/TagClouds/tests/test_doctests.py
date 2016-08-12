aa39142204951c8914f335ff1e7c49c51871b585
Adam Castle <acastle@ripe.net>
2016-08-12 14:13:06 +0200

    setting up plone.app.testing for doc tests

--- src/qi/portlet/TagClouds/tests/test_doctests.py
+++ src/qi/portlet/TagClouds/tests/test_doctests.py
@@ -1,16 +1,57 @@
-from unittest import TestSuite
 import doctest
-from Testing.ZopeTestCase import ZopeDocFileSuite
-from qi.portlet.TagClouds.tests.base import TagCloudsFunctionalTestCase
+import unittest
 
-optionflags = (doctest.REPORT_ONLY_FIRST_FAILURE |
-               doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_PASSWORD
+from plone.app.testing import applyProfile
+from plone.testing import layered
+from plone.testing.z2 import Browser
+
+from qi.portlet.TagClouds.testing import TAGCLOUD_FUNCTIONAL_TESTING
+
+optionflags = (doctest.NORMALIZE_WHITESPACE
+               | doctest.ELLIPSIS
+               | doctest.REPORT_NDIFF
+               | doctest.REPORT_ONLY_FIRST_FAILURE)
+
+
+def setUp(self):
+    layer = self.globs['layer']
+    # Update global variables within the tests.
+    self.globs.update({
+        'portal': layer['portal'],
+        'request': layer['request'],
+        'browser': Browser(layer['app']),
+        'TEST_USER_NAME': TEST_USER_NAME,
+        'TEST_USER_PASSWORD': TEST_USER_PASSWORD,
+        'self': self,
+    })
+
+    portal = self.globs['portal']
+    browser = self.globs['browser']
+
+    browser.handleErrors = True
+    portal.error_log._ignored_exceptions = ()
+
+    setRoles(portal, TEST_USER_ID, ['Manager'])
+
+    applyProfile(portal, 'Products.CMFPlone:plone')
+
+
+normal_testfiles = ['edit_after_removal.txt']
 
 
 def test_suite():
-    suite = TestSuite([
-        ZopeDocFileSuite(
-            'tests/edit_after_removal.txt', package='qi.portlet.TagClouds',
-            test_class=TagCloudsFunctionalTestCase,
-            optionflags=optionflags)])
+    suite = unittest.TestSuite()
+    suite.addTests(
+        [layered(doctest.DocFileSuite(test,
+                                      optionflags=optionflags),
+                 layer=TAGCLOUD_FUNCTIONAL_TESTING)
+         for test in normal_testfiles])
     return suite
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
