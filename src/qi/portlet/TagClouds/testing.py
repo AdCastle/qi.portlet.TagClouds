fd197b0ec3d824ac6bb848afd98ea80f3353000d
Adam Castle <acastle@ripe.net>
2016-08-15 17:37:40 +0200

    functional tests working with plone.app.testing

--- src/qi/portlet/TagClouds/testing.py
+++ src/qi/portlet/TagClouds/testing.py
@@ -2,7 +2,6 @@ from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
-from plone.app.testing import applyProfile
 from plone.testing import z2
 from Products.CMFCore.utils import getToolByName
 
@@ -14,7 +13,6 @@ class TagCloudLayer(PloneSandboxLayer):
     defaultBases = (PLONE_FIXTURE, )
 
     def setUpZope(self, app, configurationContext):
-        # Load ZCML for this package + dependencies
         import qi.portlet.TagClouds
         xmlconfig.file('configure.zcml',
                        qi.portlet.TagClouds,
@@ -22,12 +20,12 @@ class TagCloudLayer(PloneSandboxLayer):
         self.loadZCML(package=qi.portlet.TagClouds)
         z2.installProduct(app, 'qi.portlet.TagClouds')
         import plone.app.dexterity
+        self.loadZCML(name='meta.zcml', package=plone.app.dexterity)
         self.loadZCML(package=plone.app.dexterity)
 
     def setUpPloneSite(self, portal):
-        applyProfile(portal, 'Products.CMFPlone:plone')
-        applyProfile(portal, 'plone.app.contenttypes:default')
-        applyProfile(portal, 'qi.portlet.TagClouds:default')
+        self.applyProfile(portal, 'plone.app.dexterity:testing')
+        self.applyProfile(portal, 'qi.portlet.TagClouds:default')
         acl_users = getToolByName(portal, 'acl_users')
         acl_users.userFolderAddUser(
             'contributor',
