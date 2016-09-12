6a958f238b16abd3b608157ff5c550b97f22d914
Adam Castle <acastle@ripe.net>
2016-09-12 14:00:57 +0200

    Preparing release 1.36

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.36rc1'
+version = '1.36'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CHANGES.rst').read()
